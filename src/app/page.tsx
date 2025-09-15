def7a27e57d22e1e0a7d7d5c6c2635916a231299
DasFiel <jeanhsantosfiel07@gmail.com>
2025-09-15 17:10:07 -0300

    build: uso inicial de axios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+'use client';
 import "./page.css";
 import produtos from "@/mocks/produtos";
 import ProductList from "@/components/ProductList";
-import Header from "@/components/Header";
+import axios from "axios";
 
 export default function Home() {
+
+  function sucesso() {
+    alert("Deu certo");
+  }
+
+  function falha() {
+    alert("ERRO 404 - NOT FOUND!");
+  }
+
+
+  axios.get("http://10.60.46.55:4000/api/produto")
+  .then(sucesso)
+  .catch(falha)
+  .finally();
+  
 
   return ( 
     <>
